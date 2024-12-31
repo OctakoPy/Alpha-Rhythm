f9d2375518fe4a6ffbfb7fbe2c7b807330928242
OctakoPy <octakopy@gmail.com>
2024-12-31 17:53:40 +0800

    credits again fix

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
 export const Credits: React.FC = () => {
-  return (
-    <div className="fixed bottom-4 right-4 text-white/70 font-serif italic text-sm">
-      Created by Octako • © 2024
-    </div>
-  );
-};
+    return (
+      <div className="fixed bottom-4 right-4 text-white/70 font-serif italic text-sm text-center">
+        <div>Created by Octako</div>
+        <div>© 2024</div>
+      </div>
+    );
+  };
+  
